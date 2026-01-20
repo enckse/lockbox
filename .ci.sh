80589eb1862939ff6cacc3a05770cb11199d583e
Sean Enck <sean@ttypty.com>
2026-01-20 16:50:36 -0500

    remove wrapper

--- .ci.sh
+++ .ci.sh
@@ -1,11 +1,4 @@
 #!/bin/sh -e
-ENGINE=podman
-[ -n "$CONTAINER_ENGINE" ] && ENGINE="$CONTAINER_ENGINE"
-
-_ci() {
-  "$ENGINE" run --rm -v "$PWD:/app" -w /app golang:latest sh -c "make $1"
-}
-
-_ci ""
-_ci "check"
-_ci "releases"
+make
+make check
+make releases
