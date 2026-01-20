b017b5b7f8fd66dc13038c41be97f0ab09844a8b
Sean Enck <sean@ttypty.com>
2026-01-19 19:13:06 -0500

    fixing up some ci options

---
+++ .ci.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ENGINE=podman
+[ -n "$CONTAINER_ENGINE" ] && ENGINE="$CONTAINER_ENGINE"
+
+_ci() {
+  "$ENGINE" run --rm -v "$PWD:/app" -w /app golang:latest sh -c "make $1"
+}
+
+_ci ""
+_ci "check"
+_ci "releases"
